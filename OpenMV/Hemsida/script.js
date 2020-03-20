0910189de9a2f2a5f2d3c40f82e7874c2678d5f9
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-03-20 13:47:13 +0100

    Update script.js

--- OpenMV/Hemsida/script.js
+++ OpenMV/Hemsida/script.js
@@ -194,7 +194,6 @@ function pick_way(road_array, car_coord_, tile_rotation){
                 }
             }
         }
-        
         var javascriptbighomo3 = [car_coord_[0],car_coord_[1]];
         var x = indexes[Math.floor(Math.random()*indexes.length)];
         var car_coord_test = calculate_car_coords(tile_rotation, x, javascriptbighomo3);
